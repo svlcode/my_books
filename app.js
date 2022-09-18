101e72205f4fcd25164ec92195edeb93c55b7102
Vio <vio.slaniceanu@gmail.com>
2022-09-18 17:35:11 +0300

    add searchField

--- app.js
+++ app.js
@@ -4,32 +4,36 @@ new Vue({
     books: [
       {
         title: 'aa',
-        author: '',
+        author: 'albert',
         year: 0,
         readOn: '',
         notes: '',
       },
       {
         title: 'bb',
-        author: '',
+        author: 'vio',
         year: 0,
         readOn: '',
         notes: '',
       },
       {
         title: 'cc',
-        author: '',
+        author: 'aly',
         year: 0,
         readOn: '',
         notes: '',
       },
     ],
     searchValue: '',
+    searchFields: ['title', 'author', 'year'],
+    selectedField: 'title',
   },
   computed: {
     searchBooks() {
       if (this.searchValue) {
-        return this.books.filter((b) => b.title.includes(this.searchValue));
+        return this.books.filter((b) =>
+          b[this.selectedField].includes(this.searchValue)
+        );
       }
       return this.books;
     },
